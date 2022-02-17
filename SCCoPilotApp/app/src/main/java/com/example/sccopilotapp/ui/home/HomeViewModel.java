a390db96da1ea3136dd1e789e78afdb41c202352
StripedMonkey <monkeyinastripedshirt@gmail.com>
2022-02-17 12:33:49 -0500

    Add name to HomeViewModel

--- SCCoPilotApp/app/src/main/java/com/example/sccopilotapp/ui/home/HomeViewModel.java
+++ SCCoPilotApp/app/src/main/java/com/example/sccopilotapp/ui/home/HomeViewModel.java
@@ -10,7 +10,10 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue(
+                "Created By..." + "\n" +
+                "Z Noah Hounshel" + "\n"
+        );
     }
 
     public LiveData<String> getText() {
