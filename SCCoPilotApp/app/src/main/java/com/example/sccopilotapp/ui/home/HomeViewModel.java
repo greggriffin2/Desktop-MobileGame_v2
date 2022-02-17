6f3ce862be86bb0dd91cf970ef6e8cee851ff0d2
greggriffin2 <78878994+greggriffin2@users.noreply.github.com>
2022-02-17 12:35:28 -0500

    Added "created by" message to app home screen

--- SCCoPilotApp/app/src/main/java/com/example/sccopilotapp/ui/home/HomeViewModel.java
+++ SCCoPilotApp/app/src/main/java/com/example/sccopilotapp/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Created by: Greg Griffin");
     }
 
     public LiveData<String> getText() {
