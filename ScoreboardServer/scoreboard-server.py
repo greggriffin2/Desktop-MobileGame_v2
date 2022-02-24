3e4ee8bbd043df0f9fbed4ebfcfc36ab34a56e90
TomAwezome <sonidd1@yahoo.com>
2022-02-23 23:32:24 -0500

    Add docstrings to methods.

--- ScoreboardServer/scoreboard-server.py
+++ ScoreboardServer/scoreboard-server.py
@@ -2,23 +2,34 @@ from flask import Flask, request
 import json
 app = Flask(__name__)
 
-""" figure this out later i guess maybe
-@app.route('/', methods=['POST'])
-def post_endpoint():
-	pass
-"""
+""" Retrieves leaderboard.
 
+Retrieves leaderboard as a JSON object. Communicates with remote database to 
+get remote score data, then assembles into a JSON object to reply back to the
+client. 
+
+Args:
+	username (str): Username to store into remote database.
+	score (str): Game highscore to store into remote database.
+
+Returns:
+	A JSON string containing highscore data.
+
+"""
 @app.route('/retrieve/')
 def retrieve():
 	if request.method == 'GET':
 		print("RETRIEVE ATTEMPTED")
 		return "RETRIEVE ATTEMPTED"
 
-""" TODO figure this out later i guess maybe
-@app.route('/add')
-def add():
-	if request.method == 'POST':
-		pass
+""" Adds a username & score to database.
+
+Adds a username and score to a remote database as a single new entry in the table. 
+
+Args:
+	username (str): Username to store into remote database.
+	score (str): Game highscore to store into remote database.
+
 """
 @app.route('/add/<username>/<score>/')
 def add(username, score):
