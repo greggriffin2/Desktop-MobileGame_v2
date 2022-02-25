1952f1c8e28756c066d6c25fd25863f4686d83b1
StripedMonkey <monkeyinastripedshirt@gmail.com>
2022-02-24 23:43:58 -0500

    linting fixes mostly relating to spacing

--- Game/scripts/Pause.gd
+++ Game/scripts/Pause.gd
@@ -8,6 +8,7 @@ extends Control
 
 var unPaused = true
 
+
 ## _process will accept user input and process get_tree().paused based on the boolean of unPaused.
 ## This method is meant to separate pausing the game from stopping this particular process,
 ## while layering on top of and stopping all other processes.
@@ -21,16 +22,20 @@ func _process(delta):
 			get_tree().paused = false
 			unPaused = true
 			visible = false
-		
+
+
 ## Each of these buttons will perform the desired action.
+
 
 ## onContinue will resume the game.
 func onContinue():
 	pass
 
+
 ## onRestart will restart the current level.
 func onRestart():
 	pass
+
 
 ## onMenu will return the user to the main menu.
 func onMenu():
