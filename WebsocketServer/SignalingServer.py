4213d97babd114caf4b4f1a15a82eac1db734df7
StripedMonkey <monkeyinastripedshirt@gmail.com>
2022-02-25 02:05:54 -0500

    Document functions

--- WebsocketServer/SignalingServer.py
+++ WebsocketServer/SignalingServer.py
@@ -11,18 +11,35 @@ async def index(request):
     return web.Response(text="f.read()", content_type='text/html')
 
 
-@sio.event
+@sio.on("connect")
 def connect(sid, environ):
+    """
+    Handles websocket connection events
+    :param sid: socketio object
+    :param environ: environment object
+    :return:
+    """
     print("connect ", sid)
 
 
-@sio.event
+@sio.on("chat_message")
 async def chat_message(sid, data):
+    """
+    Handles messages between websockets in the same room
+    :param sid: socketio object
+    :param data: string-like data
+    :return: None
+    """
     print("message ", data)
 
 
-@sio.event
+@sio.on("disconnect")
 def disconnect(sid):
+    """
+    Handles websockets disconnecting
+    :param sid: socketio object
+    :return: None
+    """
     print('disconnect ', sid)
 
 if __name__ == '__main__':
