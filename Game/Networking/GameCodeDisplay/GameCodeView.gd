ba2f9050e6d76860082b1b7040ecf247dfc2a77f
StripedMonkey <monkeyinastripedshirt@gmail.com>
2022-03-20 22:19:28 -0400

    Implement Game Code retrieval

---
+++ Game/Networking/GameCodeDisplay/GameCodeView.gd
@@ -0,0 +1,20 @@
+extends Control
+
+var gamecode_label: RichTextLabel
+var mp_singleton: SynchronizationSingleton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	mp_singleton = get_node("/root/NetworkSynchronizationSingleton")
+	gamecode_label = get_node("VBoxContainer/HBoxContainer/GameCodeLabel")
+	mp_singleton.connect("on_gamecode_update", self, "update_gamecode")
+	update_gamecode(mp_singleton.game_code)
+
+
+func update_gamecode(code: String):
+	gamecode_label.text = code
+
+
+func _on_Reload_pressed():
+	mp_singleton.reload_connection()
