88a2ebc7f65835464b830b543914dc19d86fc597
StripedMonkey <monkeyinastripedshirt@gmail.com>
2022-04-27 10:48:24 -0400

    Unit tests to verify that serialization works Fixes #59

--- SCCoPilotApp/app/src/main/java/com/example/sccopilotapp/gamesync/UserJoined.java
+++ SCCoPilotApp/app/src/main/java/com/example/sccopilotapp/gamesync/UserJoined.java
@@ -2,7 +2,18 @@ package com.example.sccopilotapp.gamesync;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class UserJoined {
+import java.util.Objects;
+
+public class UserJoined extends DataObject {
+
+    public UserJoined(String ip) {
+        this.ip = ip;
+    }
+
+    public UserJoined() {
+
+    }
+
     String ip;
 
     @JsonProperty("ip_joined")
@@ -14,4 +25,18 @@ public class UserJoined {
     public void setIp(String ip) {
         this.ip = ip;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserJoined that = (UserJoined) o;
+        return ip.equals(that.ip);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ip);
+    }
+
 }
--- SCCoPilotApp/app/src/main/java/com/example/sccopilotapp/gamesync/UserLeft.java
+++ SCCoPilotApp/app/src/main/java/com/example/sccopilotapp/gamesync/UserLeft.java
@@ -2,6 +2,8 @@ package com.example.sccopilotapp.gamesync;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Objects;
+
 public class UserLeft {
     String ip;
 
@@ -14,4 +16,18 @@ public class UserLeft {
     public void setIp(String ip) {
         this.ip = ip;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserLeft userLeft = (UserLeft) o;
+        return ip.equals(userLeft.ip);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ip);
+    }
+
 }
